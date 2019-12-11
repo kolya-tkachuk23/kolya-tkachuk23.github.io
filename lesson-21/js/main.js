c3b0f966cfeb961ed2fe7c46617c8a6b23ccd0b8
kolya-tkachuk23 <57834002+kolya-tkachuk23@users.noreply.github.com>
2019-12-11 17:42:20 +0200

    modal_windov
    +

---
+++ lesson-21/js/main.js
@@ -0,0 +1,9 @@
+let inputTelephone = document.querySelector(".input-telephone");
+let inputName = document.querySelector(".input-name");
+let btn = document.querySelector("myBtn");
+
+btn.onclick = function () {
+	console.log("Ваш номер:" + inputName.value);
+
+
+}
